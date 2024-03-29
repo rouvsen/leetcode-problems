455a1a905086df91c4883b46b00d03d6954de251
rovshan.maharramov <rouvsen@gmail.com>
2024-03-29 14:44:52 +0400

    Add palindrome problem

--- src/main/java/org/example/problems/LeetCode_palindromenumber.java
+++ src/main/java/org/example/problems/LeetCode_palindromenumber.java
@@ -3,11 +3,15 @@ package org.example.problems;
 public class LeetCode_palindromenumber {
     public static void main(String[] args) {
         System.out.println(
-                "Entered number is palindrome: " + isPalindrome(1221)
-        );;
+                "Entered number is palindrome: " + isPalindrome(12333211)
+        );
+        System.out.println(
+                "Entered number is palindrome: " + isPalindrome2(12333211)
+        );
     }
-    public static boolean isPalindrome(int num) {
+    public static boolean isPalindrome(int num) {//TC: O(n)
         if(num < 0) return false;
+        if(num != 0 && num % 10 == 0) return false;
         int reverse = 0;
         int temp = num;
         while (temp != 0) {
@@ -16,4 +20,16 @@ public class LeetCode_palindromenumber {
         }
         return num == reverse;
     }
+    //TODO: write that TC: O(log(n))
+    public static boolean isPalindrome2(int num) {
+        if (num < 0) return false;
+        if (num != 0 && num % 10 == 0) return false;
+        int reverse = 0;
+        while(num > reverse) {
+            reverse = reverse * 10 + num % 10;
+            num /= 10;
+        }
+        return (reverse == num) || (reverse / 10 == num);
+    }
+
 }
